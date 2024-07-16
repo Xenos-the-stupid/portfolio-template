88b304b9b37e018bf517a21fda27fbe4d1bccad8
xenos <acristyano22@gmail.com>
2024-07-16 15:00:39 +0100

    add colors in tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: "var(--black)",
+        white: "var(--white)",
+        green: "var(--green)",
+        red: "var(--red)",
+        pink: "var(--pink)",
+        gray: "var(--gray)",
+        yellow: "var(--yellow)",
       },
     },
   },
