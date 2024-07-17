e1f72fa49a4d73eb29aeee78637fdabb68eab0d2
xenos <acristyano22@gmail.com>
2024-07-17 18:37:09 +0100

    adding header text in last works section

--- components/last-work.tsx
+++ components/last-work.tsx
@@ -35,24 +35,27 @@ const LAST_WORK = [
 
 const LastWork = () => {
   return (
-    <section className="min-h-[1030px] max-w-[1200px] mx-auto px-[24px] grid place-items-center grid-cols-3 grid-rows-2 gap-6 max-lg:grid-cols-2 max-sm:grid-cols-1 my-[42px]">
-      {LAST_WORK.map((work) => (
-        <div
-          key={work.image}
-          className="space-y-[24px]">
-          <Image
-            src={work.image}
-            alt="work"
-            className="mx-auto"
-            width={390}
-            height={330}
-          />
-          <div className="text-black space-y-2">
-            <h3 className="font-semibold text-xl">{work.title}</h3>
-            <p className="text-[1.0625rem]">{work.description}</p>
+    <section className="min-h-[1030px] max-w-[1200px] mx-auto px-[24px] my-[42px]">
+      <h1 className="text-[2rem] leading-[2.3125rem] font-semibold text-center mb-[42px]">Latest Work</h1>
+      <div className="grid place-items-center grid-cols-3 grid-rows-2 gap-6 max-lg:grid-cols-2 max-sm:grid-cols-1">
+        {LAST_WORK.map((work) => (
+          <div
+            key={work.image}
+            className="space-y-[24px]">
+            <Image
+              src={work.image}
+              alt="work"
+              className="mx-auto"
+              width={390}
+              height={330}
+            />
+            <div className="text-black space-y-2">
+              <h3 className="font-semibold text-xl">{work.title}</h3>
+              <p className="text-[1.0625rem]">{work.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
