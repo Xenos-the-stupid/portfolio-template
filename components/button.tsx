868b05fe075585c6cc5a660c301434252e84edeb
xenos <acristyano22@gmail.com>
2024-07-17 18:31:47 +0100

    improve focus and focus within styles

--- components/button.tsx
+++ components/button.tsx
@@ -4,7 +4,7 @@ const Button = ({ ...props }: ComponentProps<"button">) => {
   return (
     <button
       {...props}
-      className="text-white bg-black px-16 py-6 font-semibold text-xl"
+      className="text-white bg-black px-16 py-6 font-semibold text-xl duration-300 focus:ring-4 focus:ring-offset-2 focus-within:ring-4 focus-within:ring-offset-2 focus-within:outline-none ring-black"
     />
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,16 +10,28 @@ const Navbar = () => {
     <header className="max-w-[1200px] px-[42px] sticky top-0 bg-white mx-auto h-[80px] flex items-center justify-between">
       <Link
         href={"/"}
-        className="font-semibold text-xl">
+        className="font-semibold text-xl outline-none focus-within:underline duration-300 hover:scale-110 focus-within:scale-110">
         Logo
       </Link>
       <div
         className={`flex items-center gap-6 max-md:flex-col max-md:fixed max-md:inset-0 max-md:justify-center max-md:bg-[#2d2d2d]/80 max-md:text-white max-md:backdrop-blur-sm duration-300 ${
           isOpen || "max-md:translate-x-full"
         }`}>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/work"}>Work</Link>
-        <Link href={"/contact"}>Contact</Link>
+        <Link
+          href={"/about"}
+          className="duration-300 outline-none hover:scale-110 focus-within:underline focus-within:scale-110">
+          About
+        </Link>
+        <Link
+          href={"/work"}
+          className="duration-300 outline-none hover:scale-110 focus-within:underline focus-within:scale-110">
+          Work
+        </Link>
+        <Link
+          href={"/contact"}
+          className="duration-300 outline-none hover:scale-110 focus-within:underline focus-within:scale-110">
+          Contact
+        </Link>
         <button
           className="max-md:block hidden absolute top-4 right-4"
           onClick={() => setIsOpen((prev) => !prev)}>
